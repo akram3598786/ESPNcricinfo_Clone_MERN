07766d6dca4498072f137a81e7a7c3896a23324a
Manu <mangalmanu28@gmail.com>
2022-10-07 20:10:13 +0530

    done

--- projectcrickinfo/database/server.js
+++ projectcrickinfo/database/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+// import express from "express";
 import mongoose from "mongoose";
-const connection = mongoose.connect("mongodb://127.0.0.1:27017/blog");
+const connection = mongoose.connect("mongodb://127.0.0.1:27017/Cric");
 export default connection;
