88f34ee5555fb02863117a4d5e6acf0f48cfcd7a
Manu <mangalmanu28@gmail.com>
2022-10-10 14:48:53 +0530

    done

--- server/model/news.model.js
+++ server/model/news.model.js
@@ -1,19 +1,20 @@
-const mongoose  = require("mongoose")
-
+const mongoose = require("mongoose");
+//comment/
+//
 const newsSchema = new mongoose.Schema({
-    title: String,
-    keywords: [String],
-    link: String,
-    creator: [String],
-    video_url: String,
-    description: String,
-    content: String,
-    pubDate: String,
-    image_url: String,
-    source_id: String,
-    country: [String],
-    category: [String],
-    language: String
-})
+  title: String,
+  keywords: [String],
+  link: String,
+  creator: [String],
+  video_url: String,
+  description: String,
+  content: String,
+  pubDate: String,
+  image_url: String,
+  source_id: String,
+  country: [String],
+  category: [String],
+  language: String,
+});
 
-module.exports = mongoose.model("News",newsSchema)
+module.exports = mongoose.model("News", newsSchema);
